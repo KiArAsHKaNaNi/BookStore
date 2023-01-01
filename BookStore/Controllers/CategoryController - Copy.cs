8c7f17c4411e962e46a93ec90ef21a2cc5529417
KiArAsHKaNaNi <kiarash.kanani2000@gmail.com>
2023-01-02 00:33:04 +0330

    Company index and controller

---
+++ BookStore/Controllers/CategoryController - Copy.cs
@@ -0,0 +1,85 @@
+﻿using BookStore.Data.Models;
+using BookStore.Data.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var company = new Company();
+            if (id is null)
+            {
+                //This is create
+                return View(company);
+            }
+            company = _unitOfWork.Company.Get(id.GetValueOrDefault());
+            if (company is null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if (company.Id == 0)
+                {
+                    _unitOfWork.Company.Add(company);
+                }
+                else 
+                {
+                    _unitOfWork.Company.Update(company);
+                }
+                _unitOfWork.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var companyToDelete = _unitOfWork.Company.Get(id);
+            if (companyToDelete is not null)
+            {
+                _unitOfWork.Company.Remove(companyToDelete);
+                _unitOfWork.Save();
+                return Json( new { success = true, message = "Delete Successful" });
+            }
+            return Json(new { success = false, message = "Error while deleting" });
+        }
+
+
+        #region Api Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companies = _unitOfWork.Company.GetAll();
+
+            return Json(new { data = companies });
+        }
+
+        #endregion
+    }
+}
---
+++ BookStore/wwwroot/js/company.js
@@ -0,0 +1,91 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        ajax: {
+            url: "/Company/GetAll"
+        },
+        columns: [
+            { "data": "name", "width": "15%" },
+            { "data": "streetAddress", "width": "15%" },
+            { "data": "city", "width": "10%" },
+            { "data": "phoneNumber", "width": "15%" },
+            {
+                "data": "isAuthorizedCompany",
+                "render": function (data) {
+                    if (data) {
+                        return `<input type="checkbox" disabled checked />`
+                    } else {
+                        return `<input type="checkbox" disabled />`
+                    }
+                }, "width": "10%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Company/Upsert/${data}" class="btn btn-success">
+                                    Edit
+                                </a>
+                                <a onclick=Delete("/Company/Delete/${data}") class="btn btn-danger">
+                                    Delete
+                                </a>
+                            </div>
+
+                        `;
+                },
+                "width": "25%"
+            }
+        ]
+
+    });
+}
+
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then(willDelete => {
+        if (willDelete.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: data.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        dataTable.ajax.reload();
+                    } else {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'error',
+                            title: data.message,
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                }
+
+            })
+        }
+
+    })
+
+
+}
